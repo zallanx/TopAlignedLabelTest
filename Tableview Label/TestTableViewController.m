4aa7e1e177b976cbdba3b10f39d1d2662396f4c3
Daniel Galasko <daniel.galasko@02corm-p1wwfh00.local>
2015-06-24 10:00:59 +0200

    randomise our test data so we can really throttle the label

--- Tableview Label/TestTableViewController.m
+++ Tableview Label/TestTableViewController.m
@@ -9,6 +9,8 @@
 #import "TestTableViewController.h"
 #import "TestTableViewCell.h"
 
+NSString * const TestSuperLongString = @"This text appears in TopAlignedLabel. While the text does top align, the ellipses occur when the text is too long.";
+
 @interface TestTableViewController ()
 
 @property (strong, nonatomic) NSArray *totalMessages;
@@ -17,37 +19,32 @@
 
 @implementation TestTableViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    //generate random parts of our main string so that we can actually test the label
+    NSMutableArray *array = [NSMutableArray array];
+    for (int i = 0; i < 100; ++i) {
+        [array addObject:[TestSuperLongString substringToIndex:MAX(10,arc4random_uniform((uint32_t)TestSuperLongString.length))]];
+    }
+    self.totalMessages = [array copy];
+    self.tableView.rowHeight = 100;
     
-  
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-
-    // Return the number of sections.
-    return 1;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.totalMessages.count;
 }
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-
-    // Return the number of rows in the section.
-    return 1;
-}
-
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     TestTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     
-    cell.mainLabel.text = @"This text appears in TopAlignedLabel. While the text does top align, the ellipses occur when the text is too long.";
+    cell.mainLabel.text = self.totalMessages[indexPath.row];
+    cell.mainLabel.layer.borderWidth = 1;
+    cell.mainLabel.layer.borderColor = [UIColor blackColor].CGColor;
     cell.subheadingLabel.text = @"This text appears in the regular UILabel.";
     
     cell.mainLabel.numberOfLines = 0;
