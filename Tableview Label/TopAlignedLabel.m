02f71f3fcbfa7a2ee4f6c4598d1c4be03b5c0280
Daniel Galasko <daniel.galasko@02corm-p1wwfh00.local>
2015-06-24 10:01:30 +0200

    turned off the paragraph style and noticed it worked

--- Tableview Label/TopAlignedLabel.m
+++ Tableview Label/TopAlignedLabel.m
@@ -12,13 +12,11 @@
 
 - (void)drawTextInRect:(CGRect)rect {
     if (self.text) {
-        NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-        paragraphStyle.lineBreakMode = self.lineBreakMode;
         CGSize labelStringSize = [self.text boundingRectWithSize:CGSizeMake(CGRectGetWidth(self.frame), CGFLOAT_MAX)
-                                                         options:NSStringDrawingUsesLineFragmentOrigin
-                                                      attributes:@{NSFontAttributeName:self.font,NSParagraphStyleAttributeName: paragraphStyle.copy}
+                                                         options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                                      attributes:@{NSFontAttributeName:self.font}
                                                          context:nil].size;
-        [super drawTextInRect:CGRectMake(0, 0, CGRectGetWidth(self.frame),ceilf(labelStringSize.height))];
+        [super drawTextInRect:CGRectMake(0, 0, ceilf(CGRectGetWidth(self.frame)),ceilf(labelStringSize.height))];
     } else {
         [super drawTextInRect:rect];
     }
